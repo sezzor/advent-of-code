b536cfe0194f410c9fdaf50dfb5bff21f49403ff
Sebastian-PC <sezzor@live.se>
2022-12-08 14:36:01 +0100

    day8

---
+++ day8_1/Program.cs
@@ -0,0 +1,92 @@
+﻿string[] inputLines = File.ReadAllText("input.txt").Split('\n');
+int[,] array2D = new int[inputLines[0].Count(), inputLines.Count()];
+for (int r = 0; r < inputLines.Count(); r++)
+{
+    // Row
+    string row = inputLines[r];
+    for (int c = 0; c < row.Count(); c++)
+    {
+        // Column
+        array2D[r, c] = (int)row[c] - '0';
+    }
+}
+
+// Build 2D martix
+////////////////////////////////////////////////////////////////////////////////////////
+// Trees to check
+
+int visisbleTrees = 0;
+for (int row = 0; row < array2D.GetLength(0); row++)
+{
+    for (int col = 0; col < array2D.GetLength(1); col++)
+    {
+        if (IsVisible(row, col)) visisbleTrees++;
+    }
+}
+System.Console.WriteLine(visisbleTrees);
+
+// Trees to check
+////////////////////////////////////////////////////////////////////////////////////////
+// Check visibility for tree
+
+bool IsVisible(int row, int column)
+{
+    int height = array2D[row, column];
+    bool hiddenAbove = false;
+    bool hiddenBelow = false;
+    bool hiddenLeft = false;
+    bool hiddenRight = false;
+
+    // Above //////////////////
+    int above = row;
+    while (above > 0)
+    {
+        above--;
+        if (array2D[above, column] >= height)
+        {
+            hiddenAbove = true;
+            above = 0;
+        }
+    }
+    if (!hiddenAbove) return true;
+
+    // Below //////////////////
+    int below = row;
+    while (below < array2D.GetLength(0) - 1)
+    {
+        below++;
+        if (array2D[below, column] >= height)
+        {
+            hiddenBelow = true;
+            below = array2D.GetLength(0) - 1;
+        }
+    }
+    if (!hiddenBelow) return true;
+
+    // Left //////////////////
+    int left = column;
+    while (left > 0)
+    {
+        left--;
+        if (array2D[row, left] >= height)
+        {
+            hiddenLeft = true;
+            left = 0;
+        }
+    }
+    if (!hiddenLeft) return true;
+
+    // Right //////////////////
+    int right = column;
+    while (right < array2D.GetLength(1) - 1)
+    {
+        right++;
+        if (array2D[row, right] >= height)
+        {
+            hiddenRight = true;
+            right = array2D.GetLength(1) - 1;
+        }
+    }
+    if (!hiddenRight) return true;
+    else return false;
+}
---
+++ day8_2/Program.cs
@@ -0,0 +1,109 @@
+﻿string[] inputLines = File.ReadAllText("input.txt").Split('\n');
+int[,] array2D = new int[inputLines[0].Count(), inputLines.Count()];
+for (int r = 0; r < inputLines.Count(); r++)
+{
+    // Row
+    string row = inputLines[r];
+    for (int c = 0; c < row.Count(); c++)
+    {
+        // Column
+        array2D[r, c] = (int)row[c] - '0';
+    }
+}
+
+// Build 2D martix
+////////////////////////////////////////////////////////////////////////////////////////
+// Trees to check
+
+int bestScore = 0;
+for (int row = 0; row < array2D.GetLength(0); row++)
+{
+    for (int col = 0; col < array2D.GetLength(1); col++)
+    {
+        int score = ScenicScoreCalc(row, col);
+        if (score > bestScore) bestScore = score;
+    }
+}
+System.Console.WriteLine(bestScore);
+
+// Trees to check
+////////////////////////////////////////////////////////////////////////////////////////
+// Check visibility for tree
+
+int ScenicScoreCalc(int row, int column)
+{
+    int height = array2D[row, column];
+    int visAbove = 0;
+    int visBelow = 0;
+    int visLeft = 0;
+    int visRight = 0;
+
+    // Above //////////////////
+    int above = row;
+    int lastH = 0;
+    while (above > 0)
+    {
+        above--;
+        if (CanSeeNextTree(array2D[above, column], height, lastH))
+        {
+            visAbove++;
+            lastH = array2D[above, column];
+        }
+    }
+
+    // Below //////////////////
+    int below = row;
+    lastH = 0;
+    while (below < array2D.GetLength(0) - 1)
+    {
+        below++;
+        if (CanSeeNextTree(array2D[below, column], height, lastH))
+        {
+            visBelow++;
+            lastH = array2D[below, column];
+        }
+    }
+
+    // Left //////////////////
+    int left = column;
+    lastH = 0;
+    while (left > 0)
+    {
+        left--;
+        if (CanSeeNextTree(array2D[row, left], height, lastH))
+        {
+            visLeft++;
+            lastH = array2D[row, left];
+        }
+    }
+
+    // Right //////////////////
+    int right = column;
+    lastH = 0;
+    while (right < array2D.GetLength(1) - 1)
+    {
+        right++;
+        if (CanSeeNextTree(array2D[row, right], height, lastH))
+        {
+            visRight++;
+            lastH = array2D[row, right];
+        }
+    }
+
+    return (visAbove * visRight * visBelow * visLeft);
+
+    bool CanSeeNextTree(int tree, int maxH, int lastH)
+    {
+        // STUPID!!!
+        if (lastH < maxH) return true;
+        else return false;
+
+        // if (lastH == 0 && tree >= lastH) return true;
+        // else if (lastH < maxH && tree >= lastH) return true;
+        // else return false;
+
+        // FÖRRA lägre än MAXH
+        // &&
+        // TRÄD högre eller samma som FÖRRA
+    }
+}
