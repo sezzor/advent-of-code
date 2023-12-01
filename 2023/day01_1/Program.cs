6185b317d769deea0008331e8b195bf7610c5286
Sebastian-Laptop <55201584+sezzor@users.noreply.github.com>
2023-12-01 09:59:34 +0100

    2023 day 1

---
+++ 2023/day01_1/Program.cs
@@ -0,0 +1,11 @@
+﻿string text = File.ReadAllText("input.txt");
+string[] textArray = text.Split('\n');
+List<int> calibrationValues = new();
+
+foreach (var line in textArray)
+{
+    string stringNum = line.First(n => char.IsNumber(n)).ToString() + line.Last(n => char.IsNumber(n)).ToString();
+    calibrationValues.Add(int.Parse(stringNum));
+}
+
+Console.WriteLine(calibrationValues.Sum());
